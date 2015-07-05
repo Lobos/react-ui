de2a4dca941c65398154427c0dcd4a1d655f6aa1
lobos <lobos841@gmail.com>
2015-07-05 17:32:30 +0800

    rebuid

--- webpack-build-withcss.config.js
+++ webpack-build-withcss.config.js
@@ -2,9 +2,8 @@ module.exports = require("./make-webpack-config")({
   entry: {
     ReactUI: ["./src/js/index.js"]
   },
-  separateStylesheet: true,
   minimize: true,
   path: "./dist",
-  filename: '[name].js',
+  filename: '[name]-with-css.js',
   library: "[name]"
 });
