7f74b97eabf0072105554c67330ebf4fb440c4e9
lobos <lobos841@gmail.com>
2015-07-05 10:32:13 +0800

    nav

--- docs/src/js/nav-list.jsx
+++ docs/src/js/nav-list.jsx
@@ -18,14 +18,9 @@ module.exports = React.createClass({
 
   getInitialState: function () {
     return {
-      active: false,
+      active: true,
       height: 0
     }
-  },
-
-  componentDidMount: function () {
-    let height = Dom.getOuterHeight(React.findDOMNode(this))
-    this.setState({ height })
   },
 
   getClasses: function (name, route) {
@@ -37,12 +32,21 @@ module.exports = React.createClass({
   routeChange: function (route) {
     this.context.router.transitionTo(route)
     if (this.state.active) {
-      this.toggle()
+      this.close()
     }
   },
 
-  toggle: function () {
-    let active = !this.state.active
+  mouseMove: function (active) {
+    if (active !== undefined && active !== this.state.active) {
+      console.log(active)
+      this.toggle(active)
+    }
+  },
+
+  toggle: function (active) {
+    if (active === undefined) {
+      active = !this.state.active
+    }
     let rs = React.findDOMNode(this.refs.list).style
     rs.display = active ? 'block' : 'none'
 
@@ -55,6 +59,35 @@ module.exports = React.createClass({
       this.setState({ active })
       this.props.onToggle(active)
     }, 0)
+  },
+
+  show: function () {
+    if (this.state.active) {
+      return
+    }
+
+    let rs = React.findDOMNode(this.refs.list).style
+    rs.display = 'block'
+    let height = window.innerHeight || document.documentElement.clientHeight
+    let navheight = Dom.getOuterHeight(React.findDOMNode(this))
+    rs.maxHeight = (height - navheight) + 'px'
+
+    setTimeout(() => {
+      this.setState({ active: true })
+      this.props.onToggle(true)
+    }, 0)
+  },
+
+  close: function () {
+    if (this.state.active === false) {
+      return
+    }
+
+    this.setState({ active: false })
+    setTimeout(() => {
+      React.findDOMNode(this.refs.list).style.display = 'none'
+      this.props.onToggle(false)
+    }, 300)
   },
 
   getRoutesList: function (routes, index) {
@@ -87,13 +120,14 @@ module.exports = React.createClass({
     return (
       <div className={classnames("nav", {active: this.state.active})}>
         <a className="pure-menu-heading" onClick={this.routeChange.bind(this, '/')}>React UI</a>
-        <div className="nav-inner pure-menu">
-          <a onClick={this.toggle} className="nav-handler"><Icon icon="menu" size="lg" /></a>
+        <div onMouseEnter={this.show} onMouseLeave={this.close} className="nav-inner pure-menu">
+          <a onClick={this.show} className="nav-handler"><Icon icon="menu" size="lg" /></a>
           <div ref="list" className="nav-list">
+            <div className="mouse-move-pad" />
             {list}
           </div>
         </div>
-        <div onClick={this.toggle} className="overlay"></div>
+        <div onClick={this.close} className="overlay"></div>
       </div>
     )
   }
