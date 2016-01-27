5ff8a6de9bb77f438a55bdd98c58bb7e498f934f
haoliang.wu <haoliang.wu@liferay.com>
2016-01-27 14:42:53 +0800

    SF and record at 20160127

--- test/components/Select/basic.js
+++ test/components/Select/basic.js
@@ -1,4 +1,15 @@
+import Select from '../../../src/Select.js'
+
+const ReactTestUtils = React.addons.TestUtils;
+
 describe('Basic', ()=> {
+  const _defaultInstance = ReactTestUtils.renderIntoDocument(
+      <Select placeholder="single" data={['foo','bar','baz']}/>
+    ),
+    multiInstance = ReactTestUtils.renderIntoDocument(
+      <Select mult={true} data={['foo','bar','baz']}/>
+    )
+
   describe('Common', ()=> {
     it('Should render by data')
     it('Should render by placeholder')
@@ -10,7 +21,7 @@ describe('Basic', ()=> {
     it('Should render by value')
   });
 
-  describe('Mutiple Only',()=> {
+  describe('Mutiple Only', ()=> {
     it('Should render by sep')
   });
 });
