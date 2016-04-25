f9f3e8fd9db0cf3b018891e92e4257fe2bfccf6f
haoliang.wu <haoliang.wu@liferay.com>
2016-04-25 15:08:06 +0800

    implement LangSpec.js

--- test/components/Basic/LangSpec.js
+++ test/components/Basic/LangSpec.js
@@ -1,3 +1,13 @@
-import * as lang from '../../src/lang'
+import { getLang, setLang } from '../../../src/lang'
 
-describe('LangSpec', function () {})
+describe('LangSpec', () => {
+  before(() => {
+    setLang('buttons')
+  })
+
+  it('should get correct value by key', () => {
+    const value = getLang('buttons.ok')
+
+    expect(value).to.equal('确定')
+  })
+})
