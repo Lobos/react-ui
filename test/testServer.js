4a397392006d7dc2ae155e5db9459fd1095be53e
haoliang.wu <haoliang.wu@liferay.com>
2016-04-25 10:40:51 +0800

    SF

--- test/testServer.js
+++ test/testServer.js
@@ -1,30 +1,30 @@
-'use strict';
+'use strict'
 
-var path = require('path');
-var express = require('express');
-var webpack = require('webpack');
+var path = require('path')
+var express = require('express')
+var webpack = require('webpack')
 
-var config = require('../webpack.config.test');
+var config = require('../webpack.config.test')
 
-var app = express();
-var compiler = webpack(config);
+var app = express()
+var compiler = webpack(config)
 
 app.use(require('webpack-dev-middleware')(compiler, {
   noInfo: true,
   publicPath: '/'
-}));
+}))
 
-app.use(require('webpack-hot-middleware')(compiler));
+app.use(require('webpack-hot-middleware')(compiler))
 
-app.get('/', function(req, res) {
-  res.sendFile(path.join(__dirname, 'index.html'));
-});
+app.get('/', function (req, res) {
+  res.sendFile(path.join(__dirname, 'index.html'))
+})
 
-app.listen(3001, 'localhost', function(err) {
+app.listen(3001, 'localhost', function (err) {
   if (err) {
-    console.log(err);
-    return;
+    console.log(err)
+    return
   }
 
-  console.log('Listening at http://localhost:3001');
-});
+  console.log('Listening at http://localhost:3001')
+})
