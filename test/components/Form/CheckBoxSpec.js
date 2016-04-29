dc3839bcb364e023fbfc76d6ee314011a29a1709
haoliang.wu <haoliang.wu@liferay.com>
2016-04-29 11:02:58 +0800

    resolve done() called multiple times error

--- test/components/Form/CheckBoxSpec.js
+++ test/components/Form/CheckBoxSpec.js
@@ -10,15 +10,15 @@ describe('Checkbox & Checkbox Group Spec', () => {
     _defaultCheckboxGroup2 = mount(<CheckboxGroup data={compData.dataList2} />)
 
   describe('Checkbox', () => {
-    it('Should generate a label container tag', () => {
+    it('should generate a label container tag', () => {
       expect(_defaultCheckbox.find(compSelector.checkboxItem).length).to.equal(1)
     })
 
-    it('Should apply text content by text', () => {
+    it('should apply text content by text', () => {
       expect(_defaultCheckbox).to.have.text('foo')
     })
 
-    it('Should apply boolean value by default', (done) => {
+    it('should apply boolean value by default', (done) => {
       _defaultCheckbox.find('input').simulate('change', { target: { checked: true } })
       setTimeout(() => {
         expect(_defaultCheckbox.find('input')).to.have.value('true')
@@ -26,7 +26,7 @@ describe('Checkbox & Checkbox Group Spec', () => {
       }, 0)
     })
 
-    it('Should apply checked value by value', () => {
+    it('should apply checked value by value', () => {
       const wrapper1 = shallow(<Checkbox text='foo' value='foo' />),
         wrapper2 = shallow(<Checkbox text='foo' value={1} />)
 
@@ -34,22 +34,22 @@ describe('Checkbox & Checkbox Group Spec', () => {
       expect(wrapper2.instance().getValue()).to.equal(1)
     })
 
-    it('Should apply checked status by checked', () => {
+    it('should apply checked status by checked', () => {
       expect(_defaultCheckbox.find('input')).to.be.checked()
     })
   })
 
   describe('Checkbox Group', () => {
-    describe('Should render by Array data', () => {
+    describe('should render by Array data', () => {
       const checkboxItemsWrapper1 = _defaultCheckboxGroup1.find(compSelector.checkboxItem),
         checkboxItemsWrapper2 = _defaultCheckboxGroup2.find(compSelector.checkboxItem)
 
-      it('Should have the same size', () => {
+      it('should have the same size', () => {
         expect(checkboxItemsWrapper1.length).to.equal(compData.dataList1.length)
         expect(checkboxItemsWrapper2.length).to.equal(compData.dataList2.length)
       })
 
-      it('Should have the same text', () => {
+      it('should have the same text', () => {
         compData.dataList1.forEach((e, i) => {
           expect(checkboxItemsWrapper1.at(i)).to.have.text(e)
         })
@@ -60,7 +60,7 @@ describe('Checkbox & Checkbox Group Spec', () => {
       })
     })
 
-    it('Should render by inline', () => {
+    it('should render by inline', () => {
       const wrapper1 = mount(<CheckboxGroup inline data={compData.dataList1} />).find('div'),
         wrapper2 = mount(<CheckboxGroup inline={false} data={compData.dataList1} />).find('div')
 
@@ -68,8 +68,8 @@ describe('Checkbox & Checkbox Group Spec', () => {
       expect(wrapper2).to.not.have.className(compClass.checkboxGroupInlineStyle)
     })
 
-    describe('Should render by Tpl', () => {
-      it('Should render by textTpl', () => {
+    describe('should render by Tpl', () => {
+      it('should render by textTpl', () => {
         const wrapper1 = mount(<CheckboxGroup textTpl='{id}' data={compData.dataList2} />),
           checkboxItemsWrapper1 = wrapper1.find(compSelector.checkboxItem)
 
@@ -78,7 +78,7 @@ describe('Checkbox & Checkbox Group Spec', () => {
         })
       })
 
-      it('Should render by valueTpl', (done) => {
+      it('should render by valueTpl', (done) => {
         const wrapper1 = mount(<CheckboxGroup valueTpl='{text}' data={compData.dataList2} />)
         wrapper1.find('input').forEach(e => {
           e.simulate('change', { target: { checked: true } })
@@ -93,7 +93,7 @@ describe('Checkbox & Checkbox Group Spec', () => {
       })
     })
 
-    it('Should apply value by sep', function (done) {
+    it('should apply value by sep', function (done) {
       const wrapper1 = mount(<CheckboxGroup sep='|' data={compData.dataList2} />)
       wrapper1.find('input').forEach(e => {
         e.simulate('change', { target: { checked: true } })
@@ -105,7 +105,7 @@ describe('Checkbox & Checkbox Group Spec', () => {
       }, 0)
     })
 
-    it('Should apply checked status by value', () => {
+    it('should apply checked status by value', () => {
       const wrapper1 = mount(<CheckboxGroup data={compData.dataList2} value='1,2,3' />)
 
       wrapper1.find('input').forEach(e => {
@@ -114,21 +114,28 @@ describe('Checkbox & Checkbox Group Spec', () => {
     })
   })
 
-  describe('Behavior', () => {
-    it('Should call onChange callback', (done) => {
+  describe('should call onChange callback', () => {
+    it('for Checkbox', (done) => {
       const cb = () => {
           done()
         },
-        wrapper1 = shallow(<Checkbox text='foo' onChange={cb} />),
-        wrapper2 = shallow(<CheckboxGroup data={compData.dataList2} onChange={cb} />)
+        wrapper1 = shallow(<Checkbox text='foo' onChange={cb} />)
 
       wrapper1.simulate('change')
-      wrapper2.simulate('change')
+    })
+
+    it('for CheckboxGroup', (done) => {
+      const cb = () => {
+          done()
+        },
+        wrapper1 = shallow(<CheckboxGroup data={compData.dataList2} onChange={cb} />)
+
+      wrapper1.simulate('change')
     })
   })
 
   describe('Feature', () => {
-    it('Should be read only when readOnly=true', () => {
+    it('should be read only when readOnly=true', () => {
       const wrapper1 = mount(<Checkbox readOnly data={compData.dataList1} />),
         wrapper2 = mount(<CheckboxGroup readOnly data={compData.dataList2} />)
 
