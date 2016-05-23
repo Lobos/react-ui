76f21d0ad2628de50484a671bc6024c0368f1d46
haoliang.wu <haoliang.wu@liferay.com>
2016-05-23 15:47:23 +0800

    wdio init

--- gulpfile.js
+++ gulpfile.js
@@ -1,60 +1,17 @@
-const gulp = require('gulp'),
-  gutil = require('gulp-util'),
-  rimraf = require('gulp-rimraf'),
-  istanbul = require('gulp-babel-istanbul'),
-  watch = require('gulp-watch'),
-  mochaPhantomJS = require('gulp-mocha-phantomjs'),
+var gulp = require('gulp')
+var webdriver = require('gulp-webdriver')
 
-  webpack = require('webpack-stream'),
-  config = require('./webpack.config.test'),
+var src = ['src/**/*.js', 'test/e2e/**/*.js']
 
-  opts = {
-    includeUntested: true,
-    'hook-run-in-context': true,
-    reporters: ['text-summary', 'html'],
-    reportOpts: {dir: './coverage'}
-  },
-
-  paths = {
-    clean: ['./coverage', './test/static'],
-    src: ['./src/**/*.js'],
-    entry: 'test/static/testBundle.js',
-    tests: 'test/index.html'
-  };
-
-//unit test coverage
-gulp.task('clean', ()=> {
-  console.log('\nDeleting: ' + paths.clean);
-
-  return gulp.src(paths.clean, {read: false})
-    .pipe(rimraf())
-    .on('error', gutil.log);
-});
-
-gulp.task('build', ['clean'], ()=> {
-  //disable watch mode
-  config.watch=false;
-
-  return gulp.src('./').pipe(webpack(config)).pipe(gulp.dest('test'))
-});
-
-gulp.task('coverage',['build'], ()=> {
-  return gulp.src(paths.src).pipe(istanbul(opts))
-    .on('finish', ()=> {
-      gulp.src(paths.tests)
-        .pipe(mochaPhantomJS({reporter: 'spec'}))
-        .pipe(istanbul.writeReports(opts));
-    })
-    .on('error', gutil.log);
+gulp.task('webdriver:conf', function () {
+  return gulp.src('wdio.conf.js').pipe(webdriver({
+    logLevel: 'verbose',
+    waitforTimeout: 10000,
+    reporter: 'spec'
+  }))
 })
 
-//unit test
-gulp.task('test', ()=> {
-  watch(paths.entry, () => {
-    gulp
-      .src(paths.tests)
-      .pipe(mochaPhantomJS({reporter: 'spec'}));
-  });
-
-  gulp.src('./').pipe(webpack(config)).pipe(gulp.dest('test'));
-});
+gulp.task('test:e2e', function () {
+  gulp.start('webdriver:conf')
+  gulp.watch(src, ['webdriver:conf'])
+})
---
+++ test/e2e/demoSpec.js
@@ -0,0 +1,11 @@
+describe('what?', () => {
+  it('searches for WebdriverIO', () => {
+    browser.url('http://localhost:3000/#/tree')
+
+    const title = browser.getTitle()
+
+    expect(title).to.be.equal('React ui docs')
+
+    console.log('we gonna do some e2e tests for table or tree !')
+  })
+})
