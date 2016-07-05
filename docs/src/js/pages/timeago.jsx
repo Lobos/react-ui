f0cd4e2b828970ec942703d2d567a8c343943c5f
Lyon Wu <haoliang.wu@liferay.com>
2016-07-05 15:22:57 +0800

    more examples

--- docs/src/js/pages/timeago.jsx
+++ docs/src/js/pages/timeago.jsx
@@ -5,6 +5,14 @@ import Code from '../Code'
 import Example from '../Example'
 import { TimeAgo } from '../rctui'
 
+const now = new Date()
+const secondDemo = new Date(now.setSeconds(now.getSeconds() - 10))
+const minuteDemo = new Date(now.setMinutes(now.getMinutes() - 5))
+const hourDemo = new Date(now.setHours(now.getHours() - 1))
+const dayDemo = new Date(now.setDate(now.getDate() - 10))
+const monthDemo = new Date(now.setMonth(now.getMonth() - 1))
+const yearDemo = new Date(now.setFullYear(now.getFullYear() - 20))
+
 module.exports = function () {
   return (
   <div>
@@ -13,17 +21,26 @@ module.exports = function () {
     </div>
     <div className="content">
       <Code>
-{`<TimeAgo
-className={string}
-onClick={func}      // click callback
-base={date}      // 'the time base for timeago(required)'
-/>`}
+  {`<TimeAgo
+   className={string}
+   onClick={func}      // click callback
+   base={date}      // 'the time base for timeago(required)'
+   />`}
   </Code>
       <h2 className="subhead">Example</h2>
       <Example>
-        <TimeAgo base={new Date(2016, 6, 5, 0, 0, 0)} />
-        <TimeAgo base={new Date(1991, 5, 14)} />
-        <TimeAgo base={new Date(2019, 5, 14)} />
+        <span>secondDemo: {secondDemo.toLocaleString()}</span>
+        <TimeAgo base={secondDemo} />
+        <span>secondDemo: {minuteDemo.toLocaleString()}</span>
+        <TimeAgo base={minuteDemo} />
+        <span>secondDemo: {hourDemo.toLocaleString()}</span>
+        <TimeAgo base={hourDemo} />
+        <span>secondDemo: {dayDemo.toLocaleString()}</span>
+        <TimeAgo base={dayDemo} />
+        <span>secondDemo: {monthDemo.toLocaleString()}</span>
+        <TimeAgo base={monthDemo} />
+        <span>secondDemo: {yearDemo.toLocaleString()}</span>
+        <TimeAgo base={yearDemo} />
       </Example>
     </div>
   </div>
--- src/lang/zh-cn/timeago.js
+++ src/lang/zh-cn/timeago.js
@@ -4,7 +4,7 @@ module.exports = {
   week: '周',
   day: '天',
   hour: '小时',
-  minute: '分',
+  minute: '分钟',
   second: '秒',
   ago: '{0}前',
   in: '{0}后',
