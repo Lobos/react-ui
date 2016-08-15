6ec7a8c2575fe0e2f9ede0fac87f67be37563c4d
Lobos <lobos841@gmail.com>
2016-08-16 02:01:23 +0800

    'webpack-dashboard'

--- devServer.js
+++ devServer.js
@@ -3,14 +3,20 @@
 var path = require('path')
 var express = require('express')
 var webpack = require('webpack')
+var Dashboard = require('webpack-dashboard')
+var DashboardPlugin = require('webpack-dashboard/plugin')
 
 var config = require('./webpack.config.dev.js')
 
 var app = express()
 var compiler = webpack(config)
+var dashboard = new Dashboard()
+
+compiler.apply(new DashboardPlugin(dashboard.setData))
 
 app.use(require('webpack-dev-middleware')(compiler, {
-  noInfo: true,
+  // noInfo: true,
+  quiet: true,
   publicPath: '/'
 }))
 
