d89b4ec8fed82ae9c97cb1b89ddc08e9b9fb610b
Lobos <lobos841@gmail.com>
2016-08-19 00:08:12 +0800

    add Lazyload

---
+++ src/Lazyload.js
@@ -0,0 +1,40 @@
+import { Component, Children } from 'react'
+import PropTypes from './utils/proptypes'
+import { addStack, removeStack } from './utils/lazyload'
+
+export default class Lazyload extends Component {
+  constructor (props) {
+    super(props)
+    this.state = {
+      isRender: false
+    }
+
+    this._lazyId = addStack(this)
+  }
+
+  componentWillUnmount () {
+    removeStack(this._lazyId)
+  }
+
+  markToRender () {
+    this.setState({ isRender: true })
+  }
+
+  render () {
+    const { isRender } = this.state
+    const { children, placeholder } = this.props
+
+    if (!isRender) return placeholder
+
+    if (Array.isArray(children)) {
+      return <div>{children}</div>
+    } else {
+      return Children.only(children)
+    }
+  }
+}
+
+Lazyload.propTypes = {
+  children: PropTypes.any.isRquired,
+  placeholder: PropTypes.element.isRequired
+}
