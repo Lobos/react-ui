afef3283ce178d2c6aa3978730d8695ce72c1d76
Lyon Wu <haoliang.wu@liferay.com>
2016-09-05 14:58:56 +0800

    impl btn group

--- test/components/Basic/index.js
+++ test/components/Basic/index.js
@@ -1,5 +1,6 @@
 describe('Basic', function () {
   require('./ButtonSpec.js')
+  require('./ButtonGroupSpec.js')
   require('./IconSpec.js')
   require('./LangSpec.js')
   require('./SwitchSpec.js')
