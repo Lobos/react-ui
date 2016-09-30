3cf4afe1c040088301905695438594a9084310b2
tanyafuzhou <tanyafuzhou@gmail.com>
2016-09-30 09:48:06 +0800

    add media

---
+++ src/Media.js
@@ -0,0 +1,48 @@
+import classnames from 'classnames'
+
+import _styles from './styles/_media.scss'
+
+function MediaLeft (props) {
+  let classNames = classnames(_styles['media-left'], _styles[`media-${props.align}`])
+  return (
+    <div className={classNames}>
+    {props.children}
+    </div>
+  )
+}
+function MediaRight (props) {
+  let classNames = classnames(_styles['media-right'], _styles[`media-${props.align}`])
+  return (
+    <div className={classNames}>
+    {props.children}
+    </div>
+  )
+}
+function MediaBody (props) {
+  let classNames = classnames(_styles['media-body'], _styles[`media-${props.align}`])
+  return (
+    <div className={classNames}>
+    {props.children}
+    </div>
+  )
+}
+function MediaHead (props) {
+  return (
+    <div className={_styles['media-heading']}>
+    {props.children}
+    </div>
+  )
+}
+Media.Left = MediaLeft
+Media.Body = MediaBody
+Media.Head = MediaHead
+Media.Right = MediaRight
+
+export default function Media (props) {
+  return (
+    <div className={_styles.media}>
+    {props.children}
+    </div>
+  )
+};
+
