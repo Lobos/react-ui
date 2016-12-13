8c3849d5c82a1c430d53e9877790268a1c53c3c6
lobos <lobos841@gmail.com>
2016-12-13 10:45:59 +0800

    InputGroup style

--- src/InputGroup.js
+++ src/InputGroup.js
@@ -1,4 +1,4 @@
-import React, { Component, Children, cloneElement } from 'react'
+import React, {Children, cloneElement } from 'react'
 import classnames from 'classnames'
 import { getGrid } from './utils/grids'
 import Button from './Button'
@@ -6,41 +6,42 @@ import PropTypes from './utils/proptypes'
 
 import _styles from './styles/_input.scss'
 
-export default class InputGroup extends Component {
-  render () {
-    const { size, grid } = this.props
-    let className = classnames(
-      _styles.group,
-      getGrid(grid)
-    )
+export default function InputGroup (props) {
+  const { size, grid, style } = props
+  let className = classnames(
+    props.className,
+    _styles.group,
+    getGrid(grid)
+  )
 
-    const children = Children.map(this.props.children, (child) => {
-      let type = child ? child.type : null
+  const children = Children.map(props.children, (child) => {
+    let type = child ? child.type : null
 
-      if (type && type.isFormItem) {
-        return cloneElement(child, {
-          size
-        })
-      } else if (type === Button) {
-        return cloneElement(child, {
-          tag: 'a',
-          size,
-          className: classnames(this.props.className, _styles.btn)
-        })
-      } else {
-        return <div className={classnames(_styles.addon, _styles[size])}>{child}</div>
-      }
-    })
+    if (type && type.isFormItem) {
+      return cloneElement(child, {
+        size
+      })
+    } else if (type === Button) {
+      return cloneElement(child, {
+        tag: 'a',
+        size,
+        href: child.props.href || 'javascript:;',
+        className: classnames(_styles.btn)
+      })
+    } else {
+      return <div className={classnames(_styles.addon, _styles[size])}>{child}</div>
+    }
+  })
 
-    return <div className={className}>{children}</div>
-  }
+  return <div className={className} style={style}>{children}</div>
 }
 
 InputGroup.propTypes = {
   children: PropTypes.array,
   className: PropTypes.string,
   grid: PropTypes.grid,
-  size: PropTypes.oneOf(['large', 'middle', 'small'])
+  size: PropTypes.oneOf(['large', 'middle', 'small']),
+  style: PropTypes.object
 }
 
 InputGroup.defaultProps = {
